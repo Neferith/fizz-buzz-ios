bb379c104ecefc40e449565cb5fcdf00d6749199
vivien <somite100>
2022-11-26 14:52:20 +0100

    Initial Commit

---
+++ Fizzbuzz/FizzbuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzbuzzApp.swift
+//  Fizzbuzz
+//
+//  Created by Vivien on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FizzbuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
